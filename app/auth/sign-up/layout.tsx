7fcaf21352d4aef622a4a9928417acba90c7f694
RAHUL RAJ KHADKA <sirrahul9848@gmail.com>
2026-01-10 22:34:56 +0545

    added the signup form

--- app/auth/sign-up/layout.tsx
+++ app/auth/sign-up/layout.tsx
@@ -1,12 +1,22 @@
-export default function SignUpLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <>
-      <p>This is layout</p>
-      {children}
-    </>
-  );
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function AuthLayout({children}: {children: React.ReactNode}) {
+
+  return(
+<div className="min-h-screen flex itmes-center justify-center mt-40">
+    <div className="flex flex-col items-center space-y-4">
+      <Link href="/" className={buttonVariants({ variant: "ghost" })}>
+      <ArrowLeft size={30} />
+      Go Back
+      </Link>
+    </div>
+
+    <div className="w-full max-w-md mx-auto">{children}</div>
+  </div>
+
+
+  ) 
+
 }
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,11 +1,109 @@
-import React from 'react'
+"use client";
 
-const SignUp = () => {
+import { signUpSchema } from "@/app/Schemas/auth";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
+export default function SignUpPage() {
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof signUpSchema>) {
+    console.log(data);
+  }
+
   return (
-    <div>
-      <h1>Hey SignUp </h1>
-    </div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign up</CardTitle>
+        <CardDescription>Create an account to get started</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup className="gap-y-4">
+            <Controller
+              name="username"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Full Name</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Rahul Raj Khadka"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Email</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="your@gmail.com"
+                    type="email"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
 
-export default SignUp
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Password</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="*****"
+                    type="password"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Button type="submit">
+              Signup
+            </Button>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
